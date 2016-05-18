693ed5efea0d0e73220b8ea38c032e5a7fbf7fc5
Michael Zhan <michaelzhan@Michaels-MacBook-Air.local>
2016-05-18 16:41:55 -0400

    finished player factory

--- browser/js/player/player.factory.js
+++ browser/js/player/player.factory.js
@@ -1,5 +1,61 @@
 'use strict';
 
 juke.factory('PlayerFactory', function(){
+
+	var player = {};
+	
+	var audio = document.createElement('audio');
+
+	var currentSong = null;
+	var songArray = [];
+
+	player.start = function (song, songArr = null) {
+	    player.pause();
+	    audio.src = song.audioUrl;
+	    audio.load();
+	    audio.play();
+	    currentSong = song
+	    if (songArr) songArray = songArr
+	};
+
+	player.pause = function () {
+		audio.pause();
+	};
+
+	player.resume = function () {
+		audio.play();
+	};
+
+	player.isPlaying = function () {
+		return !audio.paused;
+	};
+
+	player.getCurrentSong = function () {
+		return currentSong;
+	};
+
+	player.next = function () {
+		if (songArray.indexOf(currentSong) === songArray.length - 1) {
+			player.start(songArray[0])
+		} else {
+			player.start(songArray[songArray.indexOf(currentSong) + 1])
+		}
+	};
+
+	player.previous = function () {
+		if (songArray.indexOf(currentSong) === 0) {
+			player.start(songArray[songArray.length-1])
+		} else {
+			player.start(songArray[songArray.indexOf(currentSong) - 1])
+		}
+	};
+
+	player.getProgress = function () {
+		if (!player.isPlaying()) return 0
+		else return audio.currentTime / audio.duration;	
+	};
+
+	return player;
+
   // non-UI logic in here
 });
